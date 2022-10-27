4cd7340c449e9519ae021df9522bc073941da000
AnderMed1210 <medcesar123@gmail.com>
2022-10-27 18:12:50 -0500

    Modificado desde Intellij

--- POO_Game/src/Main.java
+++ POO_Game/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 
-        /*CAMBIO - CRITERIO 11*/
+        /*CAMBIO-CRITERIO 11*/
         
         Toyota toyo = new Toyota("PAS201"); //200
         toyo.encenderMotor();
